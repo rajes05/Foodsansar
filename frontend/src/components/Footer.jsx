01ad97795be3c0cfe433d52e5d005ae270ca4b48
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-02-19 12:26:12 +0545

    added logo and contact details

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,21 @@
 import { Facebook, Twitter, Instagram, MapPin, Mail, Phone } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Logo from '../../public/combination_mark.png'
 
 function Footer() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <footer className="w-full bg-gray-900 text-gray-300 py-12 px-6 md:px-12">
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
         {/* Brand */}
-        <div className="space-y-4">
-          <h2 className="text-2xl font-bold text-[#ff4d2d]">FoodSansar</h2>
+        <div className="space-y-4"
+        onClick={()=>navigate("/")}
+        >
+          {/* <h2 className="text-2xl font-bold text-[#ff4d2d]">FoodSansar</h2> */}
+          <div className="flex items-center justify-start cursor-pointer">
+          <img src={Logo} alt="FoodSansar" className='h-10 md:h-10 object-contain' />
+          </div>
           <p className="text-sm leading-relaxed">
             Your favorite food delivery service. Fresh, fast, and delicious meals
             delivered right to your door.
@@ -44,7 +50,7 @@ function Footer() {
         <div>
           <h3 className="text-lg font-semibold text-white mb-4">Quick Links</h3>
           <ul className="space-y-2 text-sm">
-            <li onClick={()=>navigate("/about-us")}><a href="#" className="hover:text-[#ff4d2d]">About Us</a></li>
+            <li onClick={() => navigate("/about-us")}><a href="#" className="hover:text-[#ff4d2d]">About Us</a></li>
             <li><a href="#" className="hover:text-[#ff4d2d]">Our Menu</a></li>
             <li><a href="#" className="hover:text-[#ff4d2d]">Partner with Us</a></li>
             <li><a href="#" className="hover:text-[#ff4d2d]">Careers</a></li>
@@ -68,15 +74,15 @@ function Footer() {
           <ul className="space-y-3 text-sm">
             <li className="flex gap-3">
               <MapPin size={18} className="text-[#ff4d2d]" />
-              <span>123 Food Street, Culinary District, City 12345</span>
+              <span>Putalisadak, Kathmandu 44600, Nepal</span>
             </li>
             <li className="flex gap-3">
               <Phone size={18} className="text-[#ff4d2d]" />
-              <span>+1 234 567 8900</span>
+              <span>+977 9801234567</span>
             </li>
             <li className="flex gap-3">
               <Mail size={18} className="text-[#ff4d2d]" />
-              <span>support@foodsansar.com</span>
+              <span>info@foodsansar.com</span>
             </li>
           </ul>
         </div>
