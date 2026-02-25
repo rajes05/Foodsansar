d3060bba461b8776f8267740b6a0157396f46bf5
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-02-25 20:43:18 +0545

    fix: add PATCH method in cors configuration

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(cors({
         return callback(new Error(`CORS Error: Origin ${origin} is not allowed`));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type","Authorization"]
 }));
 
