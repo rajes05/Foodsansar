364d20484e4f355a1678b30d62ad52c545732c3e
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-02-28 12:56:42 +0545

    fix: using port 587 instead

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,16 +1,18 @@
 import nodemailer from 'nodemailer'; // Import nodemailer for sending emails
 import dotenv from 'dotenv'; // Import dotenv to manage environment variables
-dotenv.config(); // Load environment variables from .env file
 
 // Function to send an email
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for port 465, false for port 587
+  port: 587,
+  secure: false, // true for port 465, false for port 587
+  requireTLS: true, // Force TLS
   auth: {
     user: process.env.SMTP_EMAIL, // your gmail address
     pass: process.env.SMTP_PASS, // your gmail password or app password
   },
+  logger: true, // Log information to console
+  debug: true, // Include SMTP traffic in the logs
 });
 
 export const sendOtpMail = async (to, otp, subject = "Your OTP for Password Reset", title = "Password Reset") => {
